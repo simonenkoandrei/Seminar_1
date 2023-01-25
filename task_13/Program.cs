c2772c5a939332530bb3d5400f1292cfa1a53945
simonenkoandrei <simonenkoandrei@gmail.com>
2023-01-25 12:29:43 +0300

    дз семинара 2

---
+++ task_13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 99)
+{
+    while (a > 999)
+    {
+        a = a / 10;
+    }
+    Console.WriteLine($"Третья цифра {a % 10}");
+    }
+else Console.WriteLine("Третьей цифры нет");
